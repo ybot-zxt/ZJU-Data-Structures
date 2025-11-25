4c9088be493e9f7b557d39759b7c2a0d3f5ed58a
ybot-zxt <zhaoxintu0330@163.com>
2025-11-25 22:12:53 +0800

    栈相关操作练习

--- 02/ClassExercises.c
+++ 02/ClassExercises.c
@@ -1,26 +1,91 @@
-/*#include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
-#define MAXSIZE 100
-typedef int ElementType;
-typedef struct LinearList{
-    ElementType Data[MAXSIZE];
-    int Last;
-} LinearList, *List;
+typedef struct Node{
+    int val;
+    struct Node * Next;
+} * List;
 
-List MakeEmptyList(void);
+typedef struct stack{
+    List top;
+    List bottom;
+} STACK;
 
+void init(STACK *);
+void push(STACK *, int);
+void traverse(STACK);
+bool empty(STACK *); 
+bool pop(STACK *, int *);
 
-
-
-
-
-
-
-List MakeEmptyList(void){
-    List LPtr = (List)malloc(sizeof(struct LinearList));
-    LPtr->Last = -1;
-    return LPtr;
+int main(){
+    STACK S;
+    int val;
+    init(&S);
+    push(&S, 1);
+    push(&S, 2);
+    push(&S, 3);
+    push(&S, 4);
+    push(&S, 5);
+    traverse(S);
+    if(pop(&S, &val))
+        printf("出栈成功，出栈的元素是：%d\n", val);
+    else
+        printf("出栈失败！\n");
+    traverse(S);
+    return 0;
 }
 
-*/
+// 成功运行
+void init(STACK * ps){
+    ps->top = (List)malloc(sizeof(struct Node));
+    ps->bottom = ps->top;
+
+    ps->bottom->Next = NULL;
+
+    return ;
+}
+
+// 成功运行
+void push(STACK * ps, int val){
+    List new = (List)malloc(sizeof(struct Node));
+    new->val = val;
+
+    new->Next = ps->top;
+    ps->top = new;
+
+    return ;
+}
+
+// 成功运行
+void traverse(STACK s){
+    while(s.top != s.bottom){
+        printf("%d ", s.top->val);
+        s.top = s.top->Next;
+    }
+
+    return ;
+}
+
+// 成功运行
+bool pop(STACK * ps, int * pVal){
+    if(empty(ps)){
+        return false;
+    }
+    else{
+        List r = ps->top;
+        *pVal = r->val;
+        ps->top = ps->top->Next;
+        free(r);
+        r = NULL;
+        return true;
+    }
+}
+
+// 成功运行
+bool empty(STACK * ps){
+    if(ps->top == ps->bottom)
+        return true;
+    else
+        return false;
+}
